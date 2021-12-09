ba7062bb2ae571ae404a2196abba92c692b51774
erickcuenca <79394189+erickcuenca@users.noreply.github.com>
2021-12-09 09:30:18 -0800

    initial

---
+++ main.js
@@ -0,0 +1,10 @@
+const panels = document.querySelectorAll(".qa-container");
+panels.forEach(panel => {
+    panel.addEventListener("click", function() {
+        this.querySelector(".panel").style.maxHeight === this.querySelector(".panel").scrollHeight + "px" 
+        ? this.querySelector(".panel").style.maxHeight="0px" 
+        : this.querySelector(".panel").style.maxHeight= this.querySelector(".panel").scrollHeight + "px"
+        this.querySelector("p").classList.toggle("active-question");
+        this.querySelector(".arrow").classList.toggle("active-arrow")
+    })
+})
