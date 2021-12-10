b08088a6565bf61a652f446ec128c4c2ba3e558b
erickcuenca <79394189+erickcuenca@users.noreply.github.com>
2021-12-10 08:39:22 -0800

    fixed mobile version glitch where styles remain toggled when switching orientation

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const panels = document.querySelectorAll(".qa-container");
 panels.forEach(panel => {
     panel.addEventListener("click", function() {
-        this.querySelector(".panel").style.maxHeight === this.querySelector(".panel").scrollHeight + "px" 
+        this.querySelector(".panel").style.maxHeight != 0 && this.querySelector(".panel").style.maxHeight != "0px"
         ? this.querySelector(".panel").style.maxHeight="0px" 
         : this.querySelector(".panel").style.maxHeight= this.querySelector(".panel").scrollHeight + "px"
         this.querySelector("p").classList.toggle("active-question");
